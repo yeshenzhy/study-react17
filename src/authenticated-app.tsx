4d5afe48293fd5a3929c7673ec62fd0301f9f0fa
夜神丶 <yeshen1635@163.com>
2021-12-26 15:45:35 +0800

    修改布局2

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,11 +1,28 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
-
+import styled from "@emotion/styled";
 export const AuthenicatedApp = () => {
   const {loginOut} = useAuth();
-  return <div>
-    <button onClick={loginOut}>登出</button>
-    <ProjectListScreen></ProjectListScreen>
-  </div>
+  return <Container>
+    <PageHeader>
+      <button onClick={loginOut}>登出</button>
+    </PageHeader>
+    <Main>
+      <ProjectListScreen></ProjectListScreen>
+    </Main>
+  </Container>
 }
+
+const PageHeader = styled.header`
+  height: 6rem;
+  background-color: gray;
+`
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem calc(100vh - 6rem);
+`
